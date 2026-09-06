95d55893fcc79a350dbf9123a62caa256d0d1b57
User <User@KEYARU>
2026-09-06 22:52:04 +0800

    Update student profile outpu

--- StudentProfile/Form1.cs
+++ StudentProfile/Form1.cs
@@ -9,7 +9,7 @@ namespace StudentProfile
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Student Profile: Naoj Franzine Roldan");
+            MessageBox.Show("Student Profile — GitHub Beginner Lab");
         }
     }
 }
