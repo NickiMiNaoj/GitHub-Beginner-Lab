d69f420a0fda07a82461dbc89da7cbbe5cf6e120
Jelyn Alday <jelynalday19@outlook.com>
2026-09-13 19:21:31 +0800

    Add Student Contact Number

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -28,19 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnClick = new Button();
+            lbl1 = new Label();
             SuspendLayout();
+            // 
+            // btnClick
+            // 
+            btnClick.Location = new Point(273, 117);
+            btnClick.Name = "btnClick";
+            btnClick.Size = new Size(128, 29);
+            btnClick.TabIndex = 0;
+            btnClick.Text = "boton klik mu";
+            btnClick.UseVisualStyleBackColor = true;
+            btnClick.Click += btnClick_Click;
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Location = new Point(312, 80);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(0, 20);
+            lbl1.TabIndex = 1;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(751, 427);
+            ClientSize = new Size(858, 569);
+            Controls.Add(lbl1);
+            Controls.Add(btnClick);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnClick;
+        private Label lbl1;
     }
 }
